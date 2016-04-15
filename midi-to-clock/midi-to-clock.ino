14a7bbf4dc002476ebb1518c344f713aa9b9dc9f
David Russell <ghostcode@ghostbook.local>
2016-04-14 23:03:47 -0400

    Renamed the project and made some untested improvments

---
+++ midi-to-clock/midi-to-clock.ino
@@ -0,0 +1,89 @@
+#include <TimerOne.h>
+
+// Pins
+int clockOutput1 = 6;
+int clockOutput2 = 7;
+int divisionSwitch1 = 2;
+int divisionSwitch2 = 4;
+
+bool isPlaying = false;
+
+int clockDivision = 0;
+
+bool divisionSwitchState1 = false;
+bool divisionSwitchState2 = false;
+
+void updateDivisionMode() {
+  switchState1 = digitalRead(divisionInput1);
+  switchState2 = digitalRead(divisionInput2);
+  if (switchState1 && switchState2) clockDivision = 6; // up - sixteenth
+  else if (switchState2) 			clockDivision = 12; // middle - eigth
+  else 								clockDivision = 24; // down - quarter
+}
+
+int midiClockCount = 0;
+unsigned long lastClockMillis1 = 0;
+unsigned long lastClockMillis2 = 0;
+
+void processClock() {
+  if (midiClockCount % clockDivision == 0) {
+    digitalWrite(clockOutput1, HIGH);
+	lastClockMillis1 = millis()
+  }
+  // reset clock count every whole
+  midiClockCount = (midiClockCount + 1) % 96;
+}
+
+void startClock() {
+  isPlaying = true;
+  midiClockCount = 0;
+}
+
+void continueClock() {
+  isPlaying = true;
+}
+
+void stopClock() {
+  isPlaying = false;
+  midiClockCount = 0;
+}
+
+byte data;
+
+void processMIDI() {
+    while (Serial.available() > 0) {
+      data = Serial.read();
+      if (data == 250) startClock();
+      else if (data == 252) stopClock();
+      else if (data == 251) continueClock();
+      else if ((data == 248) && isPlaying) processClock();
+    }
+}
+
+unsigned long currentMillis = 0;
+
+void updateClockOutputs() {
+	currentMillis = millis();
+	if (currentMillis - lastClockMillis1 >= 10) {
+		pinMode(clockOutput1, LOW);
+	}
+	if (currentMillis - lastClockMillis2 >= 10) {
+		pinMode(clockOutput2, LOW);
+	}
+}
+
+void setup() {
+  Serial.begin(31250);
+  pinMode(clockOutput1, OUTPUT);
+  pinMode(clockOutput2, OUTPUT);
+  pinMode(divisionSwitch1, INPUT);
+  pinMode(divisionSwitch2, INPUT);
+}
+
+void loop() {
+  updateDivisionMode();
+  processMIDI();
+  updateClockOutputs();
+}
+
+
